a724bb7edcbe026f90003c5790c6178e75e63684
Loic Van Sluys <lvan-slu@3A3.42angouleme.fr>
2023-11-26 19:54:12 +0100

    26/11

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,12 +6,14 @@
 /*   By: lvan-slu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 13:24:52 by lvan-slu          #+#    #+#             */
-/*   Updated: 2023/11/26 18:37:01 by lvan-slu         ###   ########.fr       */
+/*   Updated: 2023/11/26 19:53:21 by lvan-slu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
+
+int	print_hexa(unsigned long long n, unsigned long long base);
 
 int	print_char(int c)
 {
@@ -62,6 +64,27 @@ int	print_digit(long n, int base, char type)
 	}	
 }
 
+/*int	print_pointeur(unsigned long long n, unsigned long long base)
+{
+	unsigned long long	i;
+	char	*symbol;
+
+	i = 0;
+	symbol = "0123456789abcdef";
+
+	if (n == 0)
+		print_hexa(n, base);
+	if (i == 0)
+		i += write (1, "0x", 2);
+	if (n < base)
+		return (print_char (symbol[n]));
+	else
+	{
+		i = print_pointeur ((n / base), base);
+		return (i + print_pointeur ((n % base), base));
+	}	
+}*/
+
 int	print_type(char type, va_list ap)
 {
 	int	i;
@@ -72,7 +95,7 @@ int	print_type(char type, va_list ap)
 	else if (type == 's')
 		i += print_str(va_arg(ap, char *));
 //	else if (type == 'p')
-//		i += print_pointeur(va_arg(ap, void *));
+//		i += print_pointeur(va_arg(ap, unsigned long long), 16);
 	else if (type == 'd')
 		i += print_digit((long)va_arg(ap, int), 10, type);
 	else if (type == 'i')
@@ -109,6 +132,9 @@ int	ft_printf(const char *format, ...)
 
 int	main()
 {
-	ft_printf("%k\n");
-	printf("%k\n");
+	int	i;
+
+	i = 425454;
+	ft_printf("%x\n", i);
+	printf("%x\n", i);
 }
