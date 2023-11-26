39b3c20957b31ee9e165dec55f96930661a1c875
Loic Van Sluys <lvan-slu@3A3.42angouleme.fr>
2023-11-26 17:27:14 +0100

    2611

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvan-slu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 13:24:52 by lvan-slu          #+#    #+#             */
+/*   Updated: 2023/11/26 17:25:48 by lvan-slu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -21,20 +33,32 @@ int	print_str(char *str)
 	return (i);
 }
 
-int	print_digit(long n, int base)
+int	print_digit(long n, int base, char type)
 {
-	int	i;
-	char	sign;
 
-	sign
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		return print_digit (-n, base) + 1;
-	}
+	int	i;
+	char	*symbol;
+
+	i = 0;
+	if (type == 'X')
+		symbol = "0123456789ABCDEF";
 	else
-		
-	
+		symbol = "0123456789abcdef";
+	if (n < 0 && (type == 'u' || type == 'X' || type == 'x'))
+		return (print_digit (n += 4294967296, base, type));
+	else if (n < 0)
+	{
+		write (1, "-", 1);
+		n *= -1;
+		return (print_digit (n, base, type));
+	}
+	else if (n < base)
+		return (print_char (symbol[n]));
+	else
+	{
+		i = print_digit ((n / base), base, type);
+		return (i + print_digit ((n % base), base, type));
+	}	
 }
 
 int	print_type(char type, va_list ap)
@@ -46,13 +70,25 @@ int	print_type(char type, va_list ap)
 		i += print_char(va_arg(ap, int));
 	else if (type == 's')
 		i += print_str(va_arg(ap, char *));
+//	else if (type == 'p')
+//		i += print_pointeur(va_arg(ap, void *));
+	else if (type == 'd')
+		i += print_digit((long)va_arg(ap, int), 10, type);
+	else if (type == 'i')
+		i += print_digit((long)va_arg(ap, int), 10, type);
+	else if (type == 'u')
+		i += print_digit((long)va_arg(ap, unsigned int), 10, type);
+	else if (type == 'x')
+		i += print_digit((long)va_arg(ap, unsigned int), 16, type);
+	else if (type == 'X')
+		i += print_digit((long)va_arg(ap, unsigned int), 16, type);
 	else if (type == '%')
 		write(1, "%", 1);
 	else write(1, &type, 1);
 	return (i);
 }
 
-int	ft_printf(const char *f, ...)
+int	ft_printf(const char *format, ...)
 {
 	va_list ap;
 	int	i;
@@ -70,10 +106,9 @@ int	ft_printf(const char *f, ...)
 	va_end(ap);
 	return (i);
 }
-
+/*
 int	main()
 {
-	char	count [] = "Je suis %%  un test";
-
-	ft_printf("Je suis a 100%%\n");
-}
+	ft_printf("%d\n", );
+	//printf("%u", 429467292);
+}*/
