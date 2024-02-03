6f8faee67a251df7e71ae588430f2f386e7e262b
Loic Van Sluys <lvan-slu@1B5.42angouleme.fr>
2024-02-03 17:22:37 +0100

    03/02/24

--- test_gnl/get_next_line.c
+++ test_gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lvan-slu <lvan-slu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 14:24:43 by lvan-slu          #+#    #+#             */
-/*   Updated: 2024/01/30 18:16:49 by lvan-slu         ###   ########.fr       */
+/*   Updated: 2024/02/03 17:15:45 by lvan-slu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,24 @@
 
 char	*free_and_store(char *str, char *buf)
 {
-	char	*tmp;
+	char	*new;
+	int	str_len;
+	int	buf_len;
 
-	tmp = ft_strjoin(str, buf);
-	free(str);
-	return (tmp);
+	buf_len = ft_strlen(buf);
+	str_len = ft_strlen(str);
+	new = ft_calloc((str_len + buf_len + 1), sizeof(char));
+	if (!buf || !str || !new)
+		return (NULL);
+	ft_memcpy(new, buf, buf_len);
+	str_len = 0;
+	while (str[str_len] != '\0')
+	{
+		new[buf_len] = str[str_len];
+		buf_len++;
+		str_len++;
+	}
+	return (new);
 }
 
 char	*sorting_buf(char *buf)
@@ -36,7 +49,7 @@ char	*sorting_buf(char *buf)
 		free(buf);
 		return (NULL);
 	}
-	line = malloc((sizeof(char) * (ft_strlen(buf) - i +1)));
+	line = ft_calloc((ft_strlen(buf) - i + 1), (sizeof(char)));
 	i++;
 	j = 0;
 	while (buf[i])
@@ -55,7 +68,7 @@ char	*sorting_line(char *buf)
 		return (NULL);
 	while (buf[i] && buf[i] != '\n')
 		i++;
-	str = malloc(sizeof(char) * (i + 1));
+	str = calloc((i + 1), sizeof(char));
 	i = 0;
 	while (buf[i] && buf[i] != '\n')
 	{
@@ -72,14 +85,16 @@ char	*read_line(int fd, char *buf)
 	char	*str;
 	int	i;
 
-	// if (!buf)
-	// 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		buf = ft_calloc((1 + 1), sizeof(char));
+	str = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	if (!str || !buf)
+		return (NULL);
 	i = 1;
 	while (i > 0)
 	{
 		i = read(fd, str, BUFFER_SIZE);
-		if (i <= 0)
+		if (i == -1)
 		{
 			free(str);
 			return (NULL);
@@ -100,11 +115,11 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buf = read_line(fd, buf); //lire fd stock buf
+	buf = read_line(fd, buf); //lire fd jusqu au 1er \n stock buf
 	if (!buf)
 		return (NULL);
-	line = sorting_line(buf); //
-	buf = sorting_buf(buf);// 
+	line = sorting_line(buf); //copier buf dans line sans \n
+	buf = sorting_buf(buf); //garder apres \n
 	return (line);
 }
 
@@ -115,5 +130,6 @@ char	*get_next_line(int fd)
 
 // 	fd = open("42_with_nl", O_RDONLY);
 
-// 	boulet = get_next_line(fd);
+// 	printf("%s", boulet = get_next_line(fd));
+// 	printf("%s", boulet = get_next_line(fd));
 // }
--- test_gnl/get_next_line_utils.c
+++ test_gnl/get_next_line_utils.c
@@ -6,75 +6,48 @@
 /*   By: lvan-slu <lvan-slu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 14:24:43 by lvan-slu          #+#    #+#             */
-/*   Updated: 2024/01/30 06:50:08 by lvan-slu         ###   ########.fr       */
+/*   Updated: 2024/02/03 17:17:09 by lvan-slu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <string.h>
 
-char *ft_strrchr(char *s, int c) 
+char	*ft_strrchr(char *s, int c)
 {
-    const char *last_occurrence = NULL;
+	char			*str;
+	unsigned char	b;
+	int				i;
 
-    // Iterate through the string until the end
-    while (*s != '\0') {
-        // If the character matches, update last occurrence
-        if (*s == (char)c) {
-            last_occurrence = s;
-        }
-        s++; // Move to the next character
-    }
-
-    // If the character was found, return its address
-    if (last_occurrence != NULL) {
-        return (char *)last_occurrence;
-    } else {
-        return NULL; // Character not found
-    }
+	str = s;
+	i = ft_strlen(str);
+	b = c;
+	while (i >= 0)
+	{
+		if (str[i] == b)
+			return (&str[i]);
+		i--;
+	}
+	return (0);
 }
 
-
-// char	*ft_strrchr(char *s, int c)
-// {
-// 	char			*str;
-// 	unsigned char	b;
-// 	int				i;
-
-// 	str = s;
-// 	i = ft_strlen(str);
-// 	b = c;
-// 	while (i >= 0)
-// 	{
-// 		if (str[i] == b)
-// 			return (&str[i]);
-// 		i--;
-// 	}
-// 	return (0);
-// }
-
-char	*ft_strjoin(char *s1, char *s2)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int		ls1;
-	int		ls2;
-	char	*news;
+	size_t	i;
+	char	*str;
+	char	*dst;
 
-	if (!s1 || !s2)
+	i = 0;
+	dst = (char *)dest;
+	str = (char *)src;
+	if (dst == 0 && str == 0)
 		return (0);
-	ls1 = ft_strlen(s1);
-	ls2 = ft_strlen(s2);
-	news = calloc((ls1 + ls2 + 1), sizeof(char));
-	if (news == NULL)
-		return (NULL);
-	memcpy(news, s1, ls1);
-	ls2 = 0;
-	while (s2[ls2] != '\0')
+	while (i < n)
 	{
-		news[ls1] = s2[ls2];
-		ls1++;
-		ls2++;
+		dst[i] = str[i];
+		i++;
 	}
-	return (news);
+	return (dst);
 }
 
 int	ft_strlen(char *str)
@@ -87,4 +60,38 @@ int	ft_strlen(char *str)
 		i++;
 	}
 	return (i);
+}
+
+char	*ft_calloc(size_t nmemb, size_t size)
+{
+	char				*ptr;
+	unsigned long long	total;
+
+	if (nmemb == 0 || size == 0)
+	{
+		nmemb = 1;
+		size = 1;
+	}
+	total = nmemb * size;
+	if (total / size != nmemb)
+		return (NULL);
+	ptr = malloc((sizeof(char) * (size + nmemb + 1)));
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, total);
+	return (ptr);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned int	i;
+	char			*str;
+
+	str = s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
 }
