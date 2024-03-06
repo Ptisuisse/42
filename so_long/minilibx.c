912ea5bad30986d26608bbe8029d5ecca6d8451b
Loic Van Sluys <lvan-slu@1B5.42angouleme.fr>
2024-03-06 11:30:23 +0100

    minilibx

--- so_long/minilibx.c
+++ so_long/minilibx.c
@@ -1,30 +1,63 @@
-#include "MacroLibX/includes/mlx.h"
-#include "get_next_line.h"
+#include "../MacroLibX/includes/mlx.h"
+#include "so_long.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 
-void    count(int fd);
+int	init_map(t_map *mapping);
 
 int main(void)
 {
+    t_map *mapping;
+
+    mapping = malloc(sizeof(t_map));
+    if (mapping == NULL)
+        return 0;
+    init_map(mapping);
     void* mlx = mlx_init();
-    void* win = mlx_new_window(mlx, 400, 400, "Hello world!");
+    void* win = mlx_new_window(mlx, 1000, 500, "Hello world!");
 
     int img_width;
     int img_height;
-    void* img = mlx_png_file_to_image(mlx, "/nfs/homes/lvan-slu/Documents/42_git/so_long/MacroLibX/res/logo.png", &img_width, &img_height);
-
-    mlx_put_image_to_window(mlx, win, img, 0, 0); // displays the image at x,y = 42,42
-    void* img2 = mlx_png_file_to_image(mlx, "/nfs/homes/lvan-slu/Documents/42_git/so_long/MacroLibX/res/logo.png", &img_width, &img_height);
-
-    mlx_put_image_to_window(mlx, win, img2, 50, 50); // displays the image at x,y = 42,42
+    void* img ;
+    mapping->y_P = 0;
+    mapping->x_P = 0;
+    while(mapping->tmp_map[mapping->y_P])
+    {
+        mapping->x_P = 0;
+        while (mapping->map[mapping->y_P][mapping->x_P])
+        {
+            if(mapping->map[mapping->y_P][mapping->x_P] == '1')
+            {
+                img = mlx_png_file_to_image(mlx, "/nfs/homes/lvan-slu/Documents/42_git/so_long/bg_cloud7.png", &img_width, &img_height);
+                mlx_put_image_to_window(mlx, win, img, (mapping->x_P * 64),(mapping->y_P * 64)); // displays the image at x,y = 42,4
+            }
+            // if(mapping->map[mapping->y_P][mapping->x_P] == '0')
+            // {
+            //     img = mlx_png_file_to_image(mlx, "/nfs/homes/lvan-slu/Documents/42_git/so_long/test.png", &img_width, &img_height);
+            //     mlx_put_image_to_window(mlx, win, img, (mapping->x_P * 32),(mapping->y_P * 32)); // displays the image at x,y = 42,4
+            // }
+            if(mapping->map[mapping->y_P][mapping->x_P] == 'P')
+            {
+                img = mlx_png_file_to_image(mlx, "/nfs/homes/lvan-slu/Documents/42_git/so_long/aviator.png", &img_width, &img_height);
+                mlx_put_image_to_window(mlx, win, img, (mapping->x_P * 64),(mapping->y_P * 64)); // displays the image at x,y = 42,4   
+            }
+            // if(mapping->map[mapping->y_P][mapping->x_P] == 'C')
+            // {
+            //     img = mlx_png_file_to_image(mlx, "/nfs/homes/lvan-slu/Documents/42_git/so_long/spr_m_aviator1", &img_width, &img_height);
+            //     mlx_put_image_to_window(mlx, win, img, (mapping->x_P * 32),(mapping->y_P * 32)); // displays the image at x,y = 42,4   
+            // }
+            // if(mapping->map[mapping->y_P][mapping->x_P] == 'E')
+            // {
+            //     img = mlx_png_file_to_image(mlx, "/nfs/homes/lvan-slu/Documents/42_git/so_long/spr_m_aviator1", &img_width, &img_height);
+            //     mlx_put_image_to_window(mlx, win, img, (mapping->x_P * 32),(mapping->y_P * 32)); // displays the image at x,y = 42,4   
+            // }
+            mapping->x_P++;
+        }
+        mapping->y_P++;
+    }
 
     mlx_loop(mlx);
-    int fd;
-    fd = open("map.ber", O_RDONLY);
-    count(fd);
-    close(fd);
 
     // do not forget to destroy the image, otherwise the MacroLibX will yell at you !
     mlx_destroy_image(mlx, img);
