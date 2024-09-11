f2eff8a7f340b9d97a5e251cba38591f36939385
Ptisuisse <van-sluys.loic@orange.fr>
2024-09-11 09:38:39 +0200

    Update [1] - 11/09

--- philosophers/init_struct.c
+++ philosophers/init_struct.c
@@ -12,51 +12,57 @@
 
 #include "philosophers.h"
 
-void init_struct(char **arg, t_init *data) 
+void	init_struct(char **arg, t_init *data)
 {
-    data->number_of_philosophers = ft_atoi(arg[1]);
-    data->philo = malloc(sizeof(t_philo) * data->number_of_philosophers);
-    data->forks = malloc(sizeof(pthread_mutex_t) * data->number_of_philosophers);
-    data->time_to_die = ft_atoi(arg[2]);
-    data->time_to_eat = ft_atoi(arg[3]);
-    data->time_to_sleep = ft_atoi(arg[4]);
+	data->number_of_philosophers = ft_atoi(arg[1]);
+	data->philo = malloc(sizeof(t_philo) * data->number_of_philosophers);
+	data->forks = malloc(sizeof(pthread_mutex_t)
+			* data->number_of_philosophers);
+	data->time_to_die = ft_atoi(arg[2]);
+	data->time_to_eat = ft_atoi(arg[3]);
+	data->time_to_sleep = ft_atoi(arg[4]);
 	if (arg[5])
 		data->number_of_times_each_philosopher_must_eat = ft_atoi(arg[5]);
-
-    pthread_mutex_init(&data->print, NULL);
+	pthread_mutex_init(&data->print, NULL);
 }
 
-void init_philo(t_init *data)
+void	init_philo(t_init *data)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < data->number_of_philosophers; i++) {
-        pthread_mutex_init(&data->forks[i], NULL);
-    }
-    i = 0;
-    while (i < data->number_of_philosophers)
-    {
-        data->philo[i].id = i + 1;
-        data->philo[i].R_fork = &data->forks[i];
-        data->philo[i].L_fork = &data->forks[(i + 1) % data->number_of_philosophers];
-        if (data->number_of_philosophers % 2)
-        {
-            data->philo[i].L_fork = &data->forks[i];
-            data->philo[i].R_fork = &data->forks[(i + 1) % data->number_of_philosophers];
-        }
-        data->philo[i].meals_eaten = 0;
-        pthread_mutex_init(&data->philo[i].last_meal, NULL);
-        //pthread_mutex_init(&data->forks[i], NULL);
-        data->philo[i].data = data;
-        if (pthread_create(&data->philo[i].thread, NULL, routine, &data->philo[i]) != 0)
-            return;
+	i = 0;
+	while (i < data->number_of_philosophers)
+	{
+		pthread_mutex_init(&data->forks[i], NULL);
         i++;
-    }
-    i = 0;
-    while (i < data->number_of_philosophers)
-    {
-        if (pthread_join(data->philo[i].thread, NULL) != 0)
-            return;
-        i++;
-    }
+	}
+	i = 0;
+	while (i < data->number_of_philosophers)
+	{
+		data->philo[i].id = i + 1;
+		data->philo[i].R_fork = &data->forks[i];
+		data->philo[i].L_fork = &data->forks[(i + 1)
+			% data->number_of_philosophers];
+		if (data->number_of_philosophers % 2)
+		{
+			data->philo[i].L_fork = &data->forks[i];
+			data->philo[i].R_fork = &data->forks[(i + 1)
+				% data->number_of_philosophers];
+		}
+		data->philo[i].meals_eaten = 0;
+		pthread_mutex_init(&data->philo[i].last_meal, NULL);
+		// pthread_mutex_init(&data->forks[i], NULL);
+		data->philo[i].data = data;
+		if (pthread_create(&data->philo[i].thread, NULL, routine,
+				&data->philo[i]) != 0)
+			return ;
+		i++;
+	}
+	i = 0;
+	while (i < data->number_of_philosophers)
+	{
+		if (pthread_join(data->philo[i].thread, NULL) != 0)
+			return ;
+		i++;
+	}
 }
